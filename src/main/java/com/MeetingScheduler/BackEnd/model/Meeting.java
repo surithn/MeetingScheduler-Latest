c1fadcab4713cc1e22e8bb7839c901316afc89b2
surithn <surithnivas97@hotmail.com>
2016-10-20 20:31:53 +0530

    Release 1.0

--- src/main/java/com/MeetingScheduler/BackEnd/model/Meeting.java
+++ src/main/java/com/MeetingScheduler/BackEnd/model/Meeting.java
@@ -14,9 +14,29 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
 @Table(name = "meeting")
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="mId")
 public class Meeting {
+
+	public List<Attendants> getAttendants() {
+		return attendants;
+	}
+
+	public void setAttendants(List<Attendants> attendants) {
+		this.attendants = attendants;
+	}
+
+	public String getmRoom() {
+		return mRoom;
+	}
+
+	public void setmRoom(String mRoom) {
+		this.mRoom = mRoom;
+	}
 
 	public String getmName() {
 		return mName;
@@ -51,34 +71,28 @@ public class Meeting {
 	}
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="m_id")
-    private int mId;
-    @Column(name="start_time")
-    private String startTime;
-    @Column(name="end_time")
-    private String endTime;
-    @Column(name="m_name")
-    private String mName;
-    
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "m_id")
+	private int mId;
+	@Column(name = "start_time")
+	private String startTime;
+	@Column(name = "end_time")
+	private String endTime;
+	@Column(name = "m_name")
+	private String mName;
+	@Column(name = "m_room")
+	private String mRoom;
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JoinTable(name = "m_emp_junction", joinColumns = {
+	@JoinTable(name = "met_att_junction", joinColumns = {
 			@JoinColumn(name = "m_id", nullable = false, updatable = false) },
-			inverseJoinColumns = { @JoinColumn(name = "e_id",
+			inverseJoinColumns = { @JoinColumn(name = "a_id",
 					nullable = false, updatable = false) })
-    private List<Employee> employees;
-    
-    @Override
-    public String toString() {
-        return String.format("Meeting[mId=%d, startTime='%s', endTime='%s']", mId, startTime, endTime);
-    }
+	private List<Attendants> attendants;
 
-	public List<Employee> getEmployees() {
-		return employees;
+	@Override
+	public String toString() {
+		return String.format("Meeting[mId=%d, startTime='%s', endTime='%s']",
+				mId, startTime, endTime);
 	}
-	
-	public void setEmployees(List<Employee> employees) {
-		this.employees = employees;
-	}
-		
+
 }
