29b086dfa628164608a7404e832b48d6155d01dc
surithn <surithnivas97@hotmail.com>
2016-10-21 09:21:58 +0530

    Update controllers.js

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -18,33 +18,43 @@ angular.module('meetingApp.controllers',[]).controller('MeetingsListController',
 
     $scope.meeting=Meeting.get({mId:$stateParams.mId});
 
-}).controller('MeetingCreateController',function($scope,$state,$stateParams,Meeting){
-
+}).controller('MeetingCreateController',function($scope,$state,$stateParams,$http,Meeting){
+	$scope.edit = false;
     $scope.meeting=new Meeting();
+	   $scope.locations=[];
+	   $scope.attendants=[];
     $scope.employees = Meeting.getEmployees(function(){});
     $scope.rooms =Meeting.getRooms(function(){});
        $scope.addMeeting=function(){
-    	$scope.meeting.attendants = $scope.attendants;
-    	$scope.meeting.mRoom = $scope.rom;
-    	$scope.meeting.$save(function(){
+    	   
+    	   angular.forEach($scope.meeting.attendants, function(value, key){
+    		    var value = value;
+    	        var parts = {};
+    	        parts[key] = value;
+    	        $scope.attendants.push(parts);
+    	    });
+    	   $scope.meeting.locations=[];
+    	   $scope.meeting.attendants=[];
+    	   //$scope.meeting.attendants = $scope.attendants;
+    	   alert(JSON.stringify($scope.meeting));
+    	   $scope.meeting.$save(function(){
             $state.go('meetings');
         });
     }
 
 }).controller('MeetingEditController',function($scope,$state,$stateParams,Meeting,popupService){
-	var urlBase="";
-    $scope.updateMeeting=function(meeting){
-    	meeting.$update(function(){
+	$scope.updateMeeting=function(){
+ 	   $scope.meeting.locations=[];
+	   $scope.meeting.attendants=[];
+		$scope.meeting.$update(function(){
     		popupService.showPopup('Meeting Details Updated!');
     		$state.go('meetings');
         });
     };
 
     $scope.loadMeeting=function(){
-        $scope.meeting=Meeting.get({mId:$stateParams.mId});
-        $scope.employees = $scope.meeting.attendants;
-        $scope.rooms = Meeting.getRooms(function(){});
+    	$scope.create = false;
+    	$scope.meeting=Meeting.get({mId:$stateParams.mId});
     };
-
-    $scope.loadMeeting();
+        $scope.loadMeeting();
 });
